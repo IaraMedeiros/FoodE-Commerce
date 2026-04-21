71022b85324c8e2924337fd053f2e8411495b431
Iara Barros <iaramedeirosbarros@gmail.com>
2026-04-20 23:41:42 -0300

    :(

--- src/test/java/com/mycompany/product/ProductRepositoryTests.java
+++ src/test/java/com/mycompany/product/ProductRepositoryTests.java
@@ -9,13 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+
 @DataJpaTest
+@SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @EntityScan(basePackages = "com.mycompany.entities")
